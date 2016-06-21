f7749522275bc81d53bead3e2622ce97da37d21e
TriiNoxYs <TriiNoxYs@gmail.com>
2016-06-21 04:50:34 +0200

    Fix respawn bug

--- src/fr/TriiNoxYs/DDHubs/listeners/events/VoidDamageEvent.java
+++ src/fr/TriiNoxYs/DDHubs/listeners/events/VoidDamageEvent.java
@@ -14,20 +14,20 @@ import fr.TriiNoxYs.DDHubs.handlers.ConfigManager;
 public class VoidDamageEvent implements Listener{
 	
     @EventHandler
-    public void onFallDamage(EntityDamageEvent e){
+    public void onVoidDamage(EntityDamageEvent e){
     	if (!(e.getEntity() instanceof Player) || e.getCause() != DamageCause.VOID)
     	    return;
     	
     	FileConfiguration config = ConfigManager.getConfig();
     	final Player p = (Player) e.getEntity();
         
-        p.teleport(new Location(
+    	p.teleport(new Location(
                 Bukkit.getWorld(config.get("spawn.world").toString()), 
-                (double) config.get("spawn.x"), 
-                (double) config.get("spawn.y"), 
-                (double) config.get("spawn.z"),
-                (float) config.get("spawn.yaw"),
-                (float) config.get("spawn.pitch")));
+                config.getDouble("spawn.x"), 
+                config.getDouble("spawn.y"), 
+                config.getDouble("spawn.z"),
+                config.getLong("spawn.yaw"),
+                config.getLong("spawn.pitch")));
     }
 
 }
